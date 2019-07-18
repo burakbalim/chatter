94e297d676185019b0dc682a3af61aed064cd407
burakb <burakb@telenity.com>
2019-07-18 16:25:32 +0300

    start chatter with docker file

--- docker/chatter/chatter-service.sh
+++ docker/chatter/chatter-service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SERVICE_NAME=chatter
-PATH_TO_JAR=deploy/chatter.messaging-1.0-SNAPSHOT.jar 
-PARAM=--configFile=file://deploy/config/chatter.json
+PATH_TO_JAR=chatter.messaging-1.0-SNAPSHOT.jar 
+PARAM=--configFile=file://config/chatter.json
 PID_PATH_NAME=/tmp/chatter-pid
 
 case $1 in
