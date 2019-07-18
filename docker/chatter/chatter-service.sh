b3663ed4f200e5559533cdc7f33f436da2e738aa
burakb <burakb@telenity.com>
2019-07-18 14:58:13 +0300

    start chatter with service

---
+++ docker/chatter/chatter-service.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+SERVICE_NAME=chatter
+PATH_TO_JAR=deploy/chatter.messaging-1.0-SNAPSHOT.jar 
+PARAM=--configFile=file://deploy/config/chatter.json
+PID_PATH_NAME=/tmp/chatter-pid
+
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup $JAVA_HOME/bin/java -jar $PATH_TO_JAR $PARAM /tmp 2>> /dev/null >> /dev/null &
+            echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac 
