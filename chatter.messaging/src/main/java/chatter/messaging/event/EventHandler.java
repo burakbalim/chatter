32b65fd9fb72b95a2e56baccf80f82a881856fc3
burakb <burakb@telenity.com>
2019-07-19 17:14:46 +0300

    start chatter with docker file

--- chatter.messaging/src/main/java/chatter/messaging/event/EventHandler.java
+++ chatter.messaging/src/main/java/chatter/messaging/event/EventHandler.java
@@ -5,11 +5,14 @@ import chatter.messaging.hazelcast.HazelcastInstanceProvider;
 import com.hazelcast.core.Message;
 import com.hazelcast.core.MessageListener;
 
+import java.sql.Time;
+import java.util.concurrent.*;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
 public abstract class EventHandler implements MessageListener<Object> {
 
+    private ExecutorService executorService = new ThreadPoolExecutor(10, 1000, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
     private static Logger logger = Logger.getLogger(EventHandler.class.getName());
 
     protected abstract void handle(Event event) throws MessageBusException;
@@ -23,13 +26,19 @@ public abstract class EventHandler implements MessageListener<Object> {
 
     @Override
     public void onMessage(Message<Object> message) {
-        Event messageObject = (Event) message.getMessageObject();
+        executorService.submit(messageHandle(message));
+    }
 
-        try {
-            handle(messageObject);
-        } catch (MessageBusException e) {
-            logger.log(Level.WARNING, "Occurred Exception in EventHandler. Exception: {0}", e);
-        }
+    private Runnable messageHandle(Message<Object> message) {
+        return () -> {
+            Event messageObject = (Event) message.getMessageObject();
+
+            try {
+                handle(messageObject);
+            } catch (MessageBusException e) {
+                logger.log(Level.WARNING, "Occurred Exception in EventHandler. Exception: {0}", e);
+            }
+        };
     }
 
     protected void onRegister(String topic){
